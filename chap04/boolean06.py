61c264e3c96ee1bcb94ba6253c604d28736a37d5
wltkdgur123@naver.com <wltkdgur123@naver.com>
2023-04-16 19:30:26 +0900

    -

--- chap04/boolean06.py
+++ chap04/boolean06.py
@@ -1,3 +1,4 @@
+#윤년 구하기
 year = int(input("Enter year: "))
 
 print(year%4==0 and year%100!=0 or year%400==0)
---
+++ chap04/boolean07.py
@@ -0,0 +1,4 @@
+#세기 구하기
+import math
+year = 2010
+print(math.trunc(year / 100 + 1) == 21)
